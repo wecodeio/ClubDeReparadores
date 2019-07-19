55ba07bf06d1be2323f475a4587d0fc4cf25e1b7
Giselle <gisecavs@gmail.com>
2019-07-19 15:09:44 -0300

    created Category table

---
+++ app/models/category.rb
@@ -0,0 +1,4 @@
+class Category < ApplicationRecord
+    validates_presence_of :name
+    validates_uniqueness_of :name
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_173948) do
+ActiveRecord::Schema.define(version: 2019_07_19_175959) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+  end
 
   create_table "repairers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name"
