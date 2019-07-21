e9943302929a2da7441bb15743eeaae9842927aa
Giselle <gisecavs@gmail.com>
2019-07-21 19:03:18 -0300

    fix show categories bugs

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,7 +10,7 @@ class CategoriesController < ApplicationController
     # GET /categories/1
     # GET /categories/1.json
     def show
-      @Category = Category.find(params[:id])
+      @category = Category.find(params[:id])
     end
   
     # GET /categories/new
@@ -42,12 +42,12 @@ class CategoriesController < ApplicationController
     # PATCH/PUT /Categories/1.json
     def update
       respond_to do |format|
-        if @Category.update(repairer_params)
-          format.html { redirect_to @Category, notice: 'Repairer was successfully updated.' }
-          format.json { render :show, status: :ok, location: @Category }
+        if @category.update(repairer_params)
+          format.html { redirect_to @category, notice: 'Repairer was successfully updated.' }
+          format.json { render :show, status: :ok, location: @category }
         else
           format.html { render :edit }
-          format.json { render json: @Category.errors, status: :unprocessable_entity }
+          format.json { render json: @category.errors, status: :unprocessable_entity }
         end
       end
     end
@@ -55,7 +55,7 @@ class CategoriesController < ApplicationController
     # DELETE /Categories/1
     # DELETE /Categories/1.json
     def destroy
-      @Category.destroy
+      @category.destroy
       respond_to do |format|
         format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
         format.json { head :no_content }
@@ -64,13 +64,13 @@ class CategoriesController < ApplicationController
   
     private
       # Use callbacks to share common setup or constraints between actions.
-      def set_Category
-        @Category = Category.find(params[:id])
+      def set_category
+        @category = Category.find(params[:id])
       end
   
       # Never trust parameters from the scary internet, only allow the white list through.
-      def Category_params
-        params.require(:repairer).permit(:first_name, :last_name, :bio, :phone, :address, :city, :mail, :cellphone)
+      def category_params
+        params.require(:repairer).permit(:name, :description)
       end
   end
   
