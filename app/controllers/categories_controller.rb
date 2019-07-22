176d45b8782dd401187508fcaad7f4c9c1f42ba2
Fran <frann.donadio@gmail.com>
2019-07-22 10:59:45 -0300

    categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,15 +25,15 @@ class CategoriesController < ApplicationController
     # POST /categories
     # POST /categories.json
     def create
-      @Category = Category.new(category_params)
+      @category = Category.new(category_params)
   
       respond_to do |format|
-        if @Category.save
-          format.html { redirect_to @Category, notice: 'Se ha creado una nueva categoria' }
-          format.json { render :show, status: :created, location: @Category }
+        if @category.save
+          format.html { redirect_to @category, notice: 'Se ha creado una nueva categoria' }
+          format.json { render :show, status: :created, location: @category }
         else
           format.html { render :new }
-          format.json { render json: @Category.errors, status: :unprocessable_entity }
+          format.json { render json: @category.errors, status: :unprocessable_entity }
         end
       end
     end
