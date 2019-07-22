597da867624c577ec63d9c52f5e9893ddd3809d7
Giselle <gisecavs@gmail.com>
2019-07-22 16:46:24 -0300

    created .env

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,4 @@
 class Admin::BaseController < ApplicationController
-    http_basic_authenticate_with name: 'admin', password: "admin"
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
     layout 'admin_application'
   end
