d38b8bda0df71c11cf00ca8d30f62ba20b6b7640
Fran <frann.donadio@gmail.com>
2019-07-26 16:43:29 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
     resources :repairers
     resources :categories
   end
-    resources :home
+   # resources :home do 
+   #   get :repairers, on: :collection
+   # end
+   resources :repairers
+   root "home#index"
+
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
