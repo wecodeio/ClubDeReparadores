172bcc33aa49616233f5e15c879413d9062fe922
Giselle <gisecavs@gmail.com>
2019-07-30 22:15:35 -0300

    cargar reparador exitosamente

--- app/controllers/repairers_controller.rb
+++ app/controllers/repairers_controller.rb
@@ -6,9 +6,27 @@ class RepairersController < ApplicationController
       @categories =  Category.all
     end
     def show
-      @category = Repairer.find(params[:id])
+      @repairer = Repairer.find(params[:id])
     end
     def new
       @repairer = Repairer.new
+    end
+    def create
+      @repairer = Repairer.new(repairer_params)
+  
+      respond_to do |format|
+        if @repairer.save
+          format.html { redirect_to @repairer, notice: 'El reparador fue exitosamente creado' }
+          format.json { render :show, status: :created, location: @repairer }
+        else
+          format.html { render :new }
+          format.json { render json: @repairer.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+    private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def repairer_params
+      params.require(:repairer).permit(:first_name, :last_name, :bio, :phone, :address, :city, :mail, :cellphone)
     end
   end
