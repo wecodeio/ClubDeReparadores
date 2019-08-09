f01f469751a93e4bab8b8bac3ad27ed63c362f78
Giselle <gisecavs@gmail.com>
2019-08-09 00:53:35 -0300

    registrarse arreglado pero iniciar sesion no :c

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,18 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    user = User.authenticate(params[:email], params[:password])
-    if user
+    user = User.find_by_email(params[:email])
+	  if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to root_path, :notice => "Sesion iniciada!"
     else
-      flash.now.alert = "Invalid email or password"
+      flash.now.alert = "Email o contraseña invalida"
       render "new"
     end
   end
   
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to root_path, :notice => "Sesion cerrada!"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,18 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
-    if @user.save
-      redirect_to root_url, :notice => "Signed up!"
-    else
-      render "new"
-    end
+	  @user = User.new(user_params)
+	  if @user.save
+	    session[:user_id] = @user.id
+	    redirect_to root_path
+	    flash.alert = "¡Gracias por registrarse!"
+	  else
+	    render "new"
+	  end
   end
+
+  private
+	  def user_params
+	    params.require(:user).permit(:email, :password, :password_confirmation)
+	  end
 end
